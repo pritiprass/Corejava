25d7c2fdfaef723c8d16297a52c1da21622b0244
Prasanna <Prasanna@LAPTOP-I96KQN81>
2020-10-28 17:41:32 +0530

    third commit

---
+++ Corejava/src/com/branch/two/BranchTwo.java
@@ -0,0 +1,11 @@
+package com.branch.two;
+
+public class BranchTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("branch two");
+	}
+
+}
